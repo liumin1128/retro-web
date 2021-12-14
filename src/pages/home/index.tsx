df27f6480de286d9d23e2fedc79078cd29df6396
liumin <min_liu@singaporeair.com.sg>
2021-12-14 12:26:37 +0800

    feat: 更新

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,16 @@
 import * as React from 'react';
-
-// interface IHomeProps {}
+import { Link } from 'umi';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
 
 const Home: React.FunctionComponent = () => {
-  return <>home</>;
+  return (
+    <Box sx={{ p: 2 }}>
+      <Link to="/retro/section">
+        <Button>retro</Button>
+      </Link>
+    </Box>
+  );
 };
 
 export default Home;
