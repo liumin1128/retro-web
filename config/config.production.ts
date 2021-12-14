3db8a37487ba1c561135784c6fb8043d7cfeadd7
liumin <min_liu@singaporeair.com.sg>
2021-12-14 08:47:30 +0800

    feat: 使用wss协议

--- config/config.production.ts
+++ config/config.production.ts
@@ -2,6 +2,6 @@ export default {
   define: {
     'process.env.API_URL': 'https://react.mobi/api',
     'process.env.GRAPHQL_URL': 'https://react.mobi/api/graphql',
-    'process.env.GRAPHQL_URL_WS': 'ws://react.mobi/api/graphql',
+    'process.env.GRAPHQL_URL_WS': 'wss://react.mobi/api/graphql',
   },
 };
