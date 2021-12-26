ba2b88ab9e1beed3c05824e92e81c6e31b85685a
liumin <min_liu@singaporeair.com.sg>
2021-12-26 13:48:13 +0800

    feat: 设置问题追踪

--- src/wrappers/material-ui/index.tsx
+++ src/wrappers/material-ui/index.tsx
@@ -2,7 +2,16 @@ import React from 'react';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { SnackbarProvider } from 'notistack';
+
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
 import theme from '@/configs/theme';
+
+Sentry.init({
+  dsn: 'https://9f28ba0a767c45ada94e165d1050e826@o115968.ingest.sentry.io/6123765',
+  integrations: [new Integrations.BrowserTracing()],
+  tracesSampleRate: 1.0,
+});
 
 interface Props {
   children: React.ReactNode;
