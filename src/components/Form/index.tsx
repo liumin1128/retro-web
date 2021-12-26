587266cbd73d9c0323a479088dce3279e640dbab
liumin <min_liu@singaporeair.com.sg>
2021-12-26 23:10:49 +0800

    feat: form组件

---
+++ src/components/Form/index.tsx
@@ -0,0 +1,122 @@
+import React, {
+  ElementType,
+  ReactNode,
+  forwardRef,
+  useImperativeHandle,
+  ForwardedRef,
+} from 'react';
+import get from 'lodash/get';
+import * as yup from 'yup';
+import { useForm, UseFormReturn } from 'react-hook-form';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import { yupResolver } from '@hookform/resolvers/yup';
+
+export interface FormRefInstance {
+  form: UseFormReturn;
+}
+
+interface Values {
+  [key: string]: string;
+}
+
+interface Item {
+  key: string;
+  label?: string;
+  placeholder?: string;
+  defaultValue?: string;
+  required?: boolean;
+  schema?: yup.AnySchema<unknown>;
+  component?: ElementType;
+  render?: (props?: Record<string, unknown>) => ReactNode;
+}
+
+interface IFormProps {
+  onSubmit?: (values: Values) => void;
+  defaultValues?: Values;
+  items: Item[];
+}
+
+const MessageForm = forwardRef(
+  (modalProps: IFormProps, ref: ForwardedRef<unknown>) => {
+    const { onSubmit, defaultValues, items } = modalProps;
+
+    // 生成schema
+    const schemaObject: Record<string, yup.AnySchema> = {};
+    items.forEach((item) => {
+      if (item.schema) {
+        schemaObject[item.key] = item.schema;
+      }
+    });
+    const schema = yup.object(schemaObject).required();
+
+    const form = useForm({
+      resolver: yupResolver(schema),
+      defaultValues,
+    });
+
+    useImperativeHandle(ref, () => ({
+      form,
+    }));
+
+    const {
+      register,
+      handleSubmit,
+      formState: { errors },
+      reset,
+    } = form;
+
+    return (
+      <form
+        onSubmit={(e) => {
+          if (onSubmit) handleSubmit(onSubmit)(e);
+          reset();
+        }}
+      >
+        <Stack
+          sx={{
+            height: '100%',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-around',
+          }}
+          spacing={2}
+        >
+          {items.map((item) => {
+            const { component, render, key, required, ...options } = item;
+
+            const Component = component || TextField;
+
+            const error = !!get(errors, key, '');
+            const helperText = get(errors, `${key}.message`, '');
+
+            const componentProps = {
+              key,
+              error,
+              helperText,
+              ...register(key, { required }),
+              ...options,
+            };
+
+            if (render) {
+              return render({ ...componentProps });
+            }
+
+            return <Component {...componentProps} />;
+          })}
+
+          {!ref && (
+            <Button fullWidth size="large" variant="contained" type="submit">
+              Send
+            </Button>
+          )}
+        </Stack>
+      </form>
+    );
+  },
+);
+
+MessageForm.defaultProps = {};
+
+export default MessageForm;
