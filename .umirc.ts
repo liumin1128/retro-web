d05209dd195acf6911956697891188435d5006f3
liumin1128 <970568830@qq.com>
2023-04-06 12:56:59 +0800

    update: config

--- .umirc.ts
+++ .umirc.ts
@@ -7,9 +7,4 @@ export default {
   npmClient: 'pnpm',
   history: { type: 'hash' },
   favicons: ['/favicon/4.svg', '/favicon/4.ico'],
-  define: {
-    'process.env.API_URL': 'http://localhost:3101',
-    'process.env.GRAPHQL_URL': 'http://localhost:3101/graphql',
-    'process.env.GRAPHQL_URL_WS': 'ws://localhost:3101/graphql',
-  },
 };
