6a2db96b3e94c58b46febbb192874c55bdb517c2
liumin1128 <970568830@qq.com>
2023-04-06 13:08:16 +0800

    update: config

---
+++ .umirc.dev.ts
@@ -0,0 +1,7 @@
+export default {
+  define: {
+    'process.env.API_URL': 'http://localhost:3101',
+    'process.env.GRAPHQL_URL': 'http://localhost:3101/graphql',
+    'process.env.GRAPHQL_URL_WS': 'ws://localhost:3101/graphql',
+  },
+};
---
+++ .umirc.production.ts
@@ -0,0 +1,7 @@
+export default {
+  define: {
+    'process.env.API_URL': 'https://react.mobi/api',
+    'process.env.GRAPHQL_URL': 'https://react.mobi/api/graphql',
+    'process.env.GRAPHQL_URL_WS': 'wss://react.mobi/api/graphql',
+  },
+};
