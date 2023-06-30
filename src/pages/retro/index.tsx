187b5b01f48f4af9d0b333b2f74fa3cbcc2ea65a
liumin1128 <970568830@qq.com>
2023-06-30 15:11:54 +0800

    update: style

--- src/pages/retro/index.tsx
+++ src/pages/retro/index.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
+import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
 import ListRetro from '@/container/Retro/List';
-import Grid from '@mui/material/Grid';
+import CreateRetro from '@/container/Retro/Create';
 
 const Retro: React.FunctionComponent = () => {
   return (
     <Container sx={{ mt: 3 }}>
-      <Grid container spacing={2}>
-        <Grid item xs={12}>
-          <ListRetro />
-        </Grid>
-      </Grid>
+      <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+        <Typography variant="h3">Retro List</Typography>
+        <CreateRetro />
+      </Box>
+
+      <br />
+      <ListRetro />
     </Container>
   );
 };
