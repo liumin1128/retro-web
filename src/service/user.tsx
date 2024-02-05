2536fe06b12706a122619fb52eadd7556bb4236a
liumin1128 <970568830@qq.com>
2024-02-05 17:20:00 +0800

    update: fix login

--- src/service/user.tsx
+++ src/service/user.tsx
@@ -2,8 +2,10 @@ import { setStorage, removeStorage, getStorage } from '@/utils/store';
 import { HREF_BEFORE_LOGOUT, USER_TOKEN } from '@/configs/base';
 
 export async function handleLogin(token: string) {
+  console.log('handleLogin: ', token);
   await setStorage(USER_TOKEN, token);
   const path = await getStorage(HREF_BEFORE_LOGOUT);
+  console.log('HREF_BEFORE_LOGOUT: ', path);
   await removeStorage(HREF_BEFORE_LOGOUT);
   window.location.href = `${path || '/'}`;
 }
